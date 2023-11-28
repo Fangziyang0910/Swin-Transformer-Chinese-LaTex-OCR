789f5cab235500f1d35c7face411b7b4854bc848
beckeason <90892677+beckeason@users.noreply.github.com>
2023-11-28 19:16:16 +0800

    Update dataset.py

--- swin-transformer-ocr/dataset.py
+++ swin-transformer-ocr/dataset.py
@@ -136,9 +136,11 @@ class CustomCollate(object):
         self.cfg = cfg
         self.tokenizer = tokenizer
         #做图像的处理，resize，数据增强，转换为tensor，我不喜欢这个resize操作
+        #将resized_image文件夹中的图像进行处理
         if is_train:
             self.transform = alb.Compose([
-                        alb.Resize(112, 448),
+                        #alb.Resize(112, 448),
+                        alb.PadPadIfNeeded(min_height=250, min_width=500,border_mode=cv2.BORDER_CONSTANT,value=[255, 255, 255]),
                         alb.ShiftScaleRotate(shift_limit=0, scale_limit=(0., 0.15), rotate_limit=1,
                             border_mode=0, interpolation=3, value=[255, 255, 255], p=0.7),
                         alb.GridDistortion(distort_limit=0.1, border_mode=0, interpolation=3,
